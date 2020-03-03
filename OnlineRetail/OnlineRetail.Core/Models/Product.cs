7f579a96270976e4623a3272e8cd6423ce00d64f
UtayaSurian <utayasurian97@gmail.com>
2020-03-03 15:42:46 +0800

    Built for Product Model and Cache

---
+++ OnlineRetail/OnlineRetail.Core/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace OnlineRetail.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }    //For Grouping/Filter Purpose
+        public string Image { get; set; }       //Using URL to store image such as path
+
+        //Contructor to automate ID
+        public Product()
+        {
+            //To generate random characters for Id automatically in database
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ OnlineRetail/OnlineRetail.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using OnlineRetail.Core;
+using OnlineRetail.Core.Models;
+                                        
+                                         //__To data from shoping cart temporarily__\\ 
+
+namespace OnlineRetail.DataAccess.InMemory
+{
+    
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;  //Object for cache
+        List<Product> products;
+
+        //constructor
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>; //To check for cache
+            if(products == null)
+            {
+                products = new List<Product>(); //then install a new cache if not exist
+            }
+     
+        }
+        //Save products in cache
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        //Add products in into cache's list called products
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+
+        //Search
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product no found!");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+            if(product != null)
+            {
+                products.Remove(product);
+            }
+            else
+            {
+                throw new Exception("Product no found!");
+            }
+        }
+    }
+}
