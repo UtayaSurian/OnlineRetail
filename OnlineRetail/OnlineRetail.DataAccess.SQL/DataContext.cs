e739eccc99ec9228cc5015cf09a035929caff261
UtayaSurian <utayasurian97@gmail.com>
2020-03-04 12:48:44 +0800

    Successfull migrated tables to SQL database using entity framework technique

---
+++ OnlineRetail/OnlineRetail.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using OnlineRetail.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRetail.DataAccess.SQL
+{                                       //Will be used to inherit from data entity framework class
+                                        //Data Migration to SQL database
+                                        //Refrence: https://www.youtube.com/watch?v=hV5QO_aJTi8 for migration
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection") //To install the connection strings for SQL Database
+        {
+
+        }
+        //Models initilizations
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
