b018cf4965b16a6db852620c24e258cbb4f2e5cf
UtayaSurian <utayasurian97@gmail.com>
2020-03-04 16:10:16 +0800

    Successfully created filtering in home page

---
+++ OnlineRetail/OnlineRetail.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineRetail.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRetail.Core.ViewModels
+{                                       //FOr filter in home page
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
