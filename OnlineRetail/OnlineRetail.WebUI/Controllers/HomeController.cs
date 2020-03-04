e756104d21d61f93792228fc70741584af0c7c5c
UtayaSurian <utayasurian97@gmail.com>
2020-03-04 15:13:18 +0800

    Added image thumbnail design in home page

--- OnlineRetail/OnlineRetail.WebUI/Controllers/HomeController.cs
+++ OnlineRetail/OnlineRetail.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineRetail.Core.Contracts;
+using OnlineRetail.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,31 @@ namespace OnlineRetail.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context; //Object to call all methods delcared in InMemory
+        IRepository<ProductCategory> productCategories; //To load all categories from the database for drop-down list
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoriesContext)
+        {
+            //Initialize the object automatically
+            context = productContext;
+            productCategories = productCategoriesContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
-
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
