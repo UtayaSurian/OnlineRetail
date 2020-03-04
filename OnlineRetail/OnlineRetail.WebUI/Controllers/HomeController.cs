1057c6c68c71a6490f466cafd800fc696ecc395b
UtayaSurian <utayasurian97@gmail.com>
2020-03-04 16:10:16 +0800

    Successfully created filtering in home page

--- OnlineRetail/OnlineRetail.WebUI/Controllers/HomeController.cs
+++ OnlineRetail/OnlineRetail.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using OnlineRetail.Core.Contracts;
 using OnlineRetail.Core.Models;
+using OnlineRetail.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,27 @@ namespace OnlineRetail.WebUI.Controllers
             context = productContext;
             productCategories = productCategoriesContext;
         }
-        public ActionResult Index()
+
+        //Filter for home page
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products=context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+               
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
         public ActionResult Details(string Id)
         {
