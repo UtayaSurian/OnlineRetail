915ddd380063bedf9c7d722a8372a2f8bb73e5e8
UtayaSurian <utayasurian97@gmail.com>
2020-03-04 21:16:34 +0800

    CartService for Add and Remove

--- OnlineRetail/OnlineRetail.Core/Models/CartItem.cs
+++ OnlineRetail/OnlineRetail.Core/Models/CartItem.cs
@@ -8,7 +8,7 @@ namespace OnlineRetail.Core.Models
 {
     public class CartItem : BaseEntity
     {
-        public string BasketId { get; set; }
+        public string CartId { get; set; }
         public string ProductId { get; set; }     //To dynamically change the price when manager change the actual product price
         public int Quantity { get; set; }
     }
--- OnlineRetail/OnlineRetail.Services/CartService.cs
+++ OnlineRetail/OnlineRetail.Services/CartService.cs
@@ -14,40 +14,28 @@ namespace OnlineRetail.Services
         IRepository<Product> productContext;
         IRepository<Cart> cartContext;
 
-        //Cookie 
+        //Cookie
         //Fixed
-        public const string CartSessionName = "Mobile&WebMultimedia";
+        public const string CartSessionName = "MobileWebMultimedia";
 
         public CartService(IRepository<Product> ProductContext, IRepository<Cart> CartContext)
         {
             this.productContext = ProductContext;
             this.cartContext = CartContext;
         }
-        //Assining cookies in newly created cart
-        private Cart CreateNewCart(HttpContextBase httpContext)
-        {
-            Cart cart = new Cart();
-            cartContext.Insert(cart);
-            cartContext.Commit();
 
-            HttpCookie cookie = new HttpCookie(CartSessionName);
-            cookie.Value = cart.Id;
-            cookie.Expires = DateTime.Now.AddDays(2); //How many days for the cookie to be active
-            httpContext.Response.Cookies.Add(cookie);
-
-            return cart;
-        }
-
-
+        //Load the cart via cart id
         private Cart GetCart(HttpContextBase httpContext, bool createIfNull)
         {
+            //Look for available cookie
             HttpCookie cookie = httpContext.Request.Cookies.Get(CartSessionName);
 
             Cart cart = new Cart();
-            if (cookie != null){
+            if(cookie != null)
+            {
                 string cartId = cookie.Value;
-                if (!string.IsNullOrEmpty(cartId))  //If not empty
-                {
+                if (!string.IsNullOrEmpty(cartId))
+                {   //Load the cart
                     cart = cartContext.Find(cartId);
                 }
                 else
@@ -60,22 +48,35 @@ namespace OnlineRetail.Services
             }
             else
             {
+
                 if (createIfNull)
                 {
                     cart = CreateNewCart(httpContext);
                 }
             }
+            return cart;
+        }
+
+        private Cart CreateNewCart(HttpContextBase httpContext)
+        {
+            Cart cart = new Cart();
+            cartContext.Insert(cart);
+            cartContext.Commit();
+
+            HttpCookie cookie = new HttpCookie(CartSessionName);
+            cookie.Value = cart.Id;
+            cookie.Expires = DateTime.Now.AddMonths(1); //Cookie expiration
+            httpContext.Response.Cookies.Add(cookie);
 
             return cart;
         }
 
-        // add items in backet
         public void AddToCart(HttpContextBase httpContext, string productId)
         {
-            Cart cart = GetCart(httpContext, true); //load the cart from the database
-            CartItem item = cart.CartItems.FirstOrDefault(i => i.ProductId == productId);
+            Cart cart = GetCart(httpContext, true);  //Load from the database
+            CartItem item = cart.CartItems.FirstOrDefault(i=>i.ProductId == productId);
 
-            //If the item exist in the basket, do the following:
+
             if (item == null)
             {
                 item = new CartItem()
@@ -85,14 +86,29 @@ namespace OnlineRetail.Services
                     Quantity = 1
                 };
 
-              //  cart = CartItems.Add(item);
+                cart.CartItems.Add(item);
+
+
             }
             else
-            {
-                item.Quantity = item.Quantity + 1;     //Add item to currect existing cart
+            {   //Implement the property
+                item.Quantity = item.Quantity + 1; //update the value 
             }
 
             cartContext.Commit();
+        }
+
+        public void RemoveFromCart(HttpContextBase httpContext, string itemId)
+        {
+            Cart cart = GetCart(httpContext, true);
+            CartItem item = cart.CartItems.FirstOrDefault(i => i.Id == itemId);
+
+            if(item != null)
+            {
+                cart.CartItems.Remove(item);
+                cartContext.Commit();
+            }
+
         }
     }
 }
